c9f444d3148cc24554a85ea865a5dca45794986c
mrzhuzhe <zhuzhe0815@gmail.com>
2025-10-08 21:57:26 +0800

    dagtogatsel

--- test/test.sh
+++ test/test.sh
@@ -4,7 +4,8 @@ bin/clang -target mips-unknown-linux-gnu -c /home/chernoalpha/Code/lbdex/input/c
 #  chapter 3_1 error ... Assertion failed: (MRI && "Unable to create reg info"), function initAsmInfo
 #  chapter 3_2 error does not support generation of this file type!
 #  chapter 3_3 LLVM ERROR: Cannot select: t6: ch = Cpu0ISD::Ret t4, Register:i32 $lr
-bin/llc -march=cpu0 -relocation-model=pic -filetype=asm ch3.bc -o ch3.cpu0.s
+bin/llc -march=cpu0 -relocation-model=pic -filetype=asm ch3.bc -debug-pass=Structure -o ch3.cpu0.s
+# bin/llc -march=cpu0 -relocation-model=pic -filetype=asm ch3.bc -debug-pass=Structure -o ch3.cpu0.s
 
 #   bin/llvm-dis ch3.bc -o -
 #   bin/llc -march=cpu0 -mcpu=help
