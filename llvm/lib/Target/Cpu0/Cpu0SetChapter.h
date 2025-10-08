1950ee5b89c7c82022bcfcc80ac14029f9fb2969
mrzhuzhe <zhuzhe0815@gmail.com>
2025-10-08 21:51:50 +0800

    chapter 3_3

--- llvm/lib/Target/Cpu0/Cpu0SetChapter.h
+++ llvm/lib/Target/Cpu0/Cpu0SetChapter.h
@@ -1,2 +1,2 @@
-#define CH       CH3_2
+#define CH       CH3_3
 
--- test/test.sh
+++ test/test.sh
@@ -3,6 +3,7 @@ bin/clang -target mips-unknown-linux-gnu -c /home/chernoalpha/Code/lbdex/input/c
 #  chapter 2 error Could not allocate target machine!
 #  chapter 3_1 error ... Assertion failed: (MRI && "Unable to create reg info"), function initAsmInfo
 #  chapter 3_2 error does not support generation of this file type!
+#  chapter 3_3 LLVM ERROR: Cannot select: t6: ch = Cpu0ISD::Ret t4, Register:i32 $lr
 bin/llc -march=cpu0 -relocation-model=pic -filetype=asm ch3.bc -o ch3.cpu0.s
 
 #   bin/llvm-dis ch3.bc -o -
