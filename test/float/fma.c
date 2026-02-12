1e90ee18da34633e70cae734400de307dfeea095
mrzhuzhe <466648706@qq.com>
2026-02-12 21:27:18 +0800

    pattern

---
+++ test/float/fma.c
@@ -0,0 +1,16 @@
+/*
+    16688 = 0x4130 = 11 float format
+    16816 = 0x41b0 = 22 float format
+*/
+
+int main(){
+    float a = 11; 
+    float b = 22;
+    float c = a * b;
+
+    // unsigned short a = 11; 
+    // unsigned short b = 22;
+    // unsigned short c = a * b;
+
+    return 0;
+}
