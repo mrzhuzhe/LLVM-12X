5c45278018b59016cc330429a4b3def5bfc72953
mrzhuzhe <466648706@qq.com>
2026-02-14 20:33:23 +0800

    call conv

--- test/float/comparefunction.c
+++ test/float/comparefunction.c
@@ -2,6 +2,7 @@
     16688 = 0x4130 = 11 float format
     16816 = 0x41b0 = 22 float format
 */
+#include "stdio.h"
 
 int func1(int a, int b){
     return a*b;
@@ -14,5 +15,6 @@ int main(){
     // int e = 44;
     int c = func1(a, b);
     // d = c + e;
+    printf("teststr %d", c);
     return 0;
 }
